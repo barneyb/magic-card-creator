2f76f7be5f17a8d1e340d835e5e947b033c5b4dc
barneyb <bboisvert@gmail.com>
2014-06-22 14:39:34 -0700

    reimplemented getImageAsset to use ClasspathImage, instead of an anonymous implementation of ImageAsset

--- src/main/java/com/barneyb/magic/creator/asset/AssetSet.groovy
+++ src/main/java/com/barneyb/magic/creator/asset/AssetSet.groovy
@@ -1,10 +1,7 @@
 package com.barneyb.magic.creator.asset
 import com.barneyb.magic.creator.descriptor.AssetKeyed
 
-import javax.imageio.ImageIO
 import java.awt.*
-import java.awt.image.BufferedImage
-
 /**
  *
  * @author bboisvert
@@ -20,39 +17,6 @@ class AssetSet {
     String type
 
     ImageAsset getImageAsset(AssetKeyed keyed) {
-        new ImageAsset() {
-            @Override
-            Dimension getSize() {
-                AssetSet.this.size
-            }
-
-            @Override
-            BufferedImage asImage() {
-                def path = assetPath
-                def stream = RenderSet.classLoader.getResourceAsStream(path)
-                if (stream == null) {
-                    throw new IllegalArgumentException("No image could be read from the classpath at '$path'.")
-                }
-                ImageIO.read(stream)
-            }
-
-            private String getAssetPath() {
-                "assets/$renderSet.key/$key/${keyed.assetKey}.$type"
-            }
-
-            String toString() {
-                '{' + keyed.assetKey + '}'
-            }
-
-            @Override
-            int hashCode() {
-                assetPath.hashCode()
-            }
-
-            @Override
-            boolean equals(Object o) {
-                getClass().isAssignableFrom(o.getClass()) && assetPath == o.assetPath
-            }
-        }
+        new ClasspathImage("assets/$renderSet.key/$key/${keyed.assetKey}.$type")
     }
 }
