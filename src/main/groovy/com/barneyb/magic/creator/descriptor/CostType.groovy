445ade7fe806831a9a1d2b3d717591819ef047ba
barneyb <bboisvert@gmail.com>
2014-06-23 09:06:44 -0700

    added color associations to cost types

--- src/main/groovy/com/barneyb/magic/creator/descriptor/CostType.groovy
+++ src/main/groovy/com/barneyb/magic/creator/descriptor/CostType.groovy
@@ -6,28 +6,34 @@ package com.barneyb.magic.creator.descriptor
  */
 enum CostType implements AssetKeyed {
 
-    BLACK('b'),
-    BLUE('u'),
-    COLORLESS_0('0'),
-    COLORLESS_1('1'),
-    COLORLESS_2('2'),
-    COLORLESS_3('3'),
-    COLORLESS_4('4'),
-    COLORLESS_5('5'),
-    COLORLESS_6('6'),
-    COLORLESS_7('7'),
-    COLORLESS_8('8'),
-    COLORLESS_9('9'),
-    COLORLESS_X('x'),
-    GREEN('g'),
-    RED('r'),
-    TAP('t'),
-    WHITE('w'),
+    COLORLESS_0('0', ManaColor.COLORLESS),
+    COLORLESS_1('1', ManaColor.COLORLESS),
+    COLORLESS_2('2', ManaColor.COLORLESS),
+    COLORLESS_3('3', ManaColor.COLORLESS),
+    COLORLESS_4('4', ManaColor.COLORLESS),
+    COLORLESS_5('5', ManaColor.COLORLESS),
+    COLORLESS_6('6', ManaColor.COLORLESS),
+    COLORLESS_7('7', ManaColor.COLORLESS),
+    COLORLESS_8('8', ManaColor.COLORLESS),
+    COLORLESS_9('9', ManaColor.COLORLESS),
+    COLORLESS_X('x', ManaColor.COLORLESS),
+    WHITE('w', ManaColor.WHITE),
+    BLUE('u', ManaColor.BLUE),
+    BLACK('b', ManaColor.BLACK),
+    RED('r', ManaColor.RED),
+    GREEN('g', ManaColor.GREEN),
+    TAP('t', []),
 
     final String assetKey
+    final Collection<ManaColor> colors
 
-    def CostType(String assetKey) {
+    def CostType(String assetKey, ManaColor color) {
+        this(assetKey, [color])
+    }
+
+    def CostType(String assetKey, Collection<ManaColor> colors) {
         this.assetKey = assetKey
+        this.colors = colors
     }
 
     static CostType fromKey(String key) {
