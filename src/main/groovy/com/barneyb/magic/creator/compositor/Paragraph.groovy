7b75c774fcdd91aa35894ebe660592871aed5945
barneyb <bboisvert@gmail.com>
2014-06-23 16:40:10 -0700

    put a toString on Paragraph so it shows up as a paragraph symbol

--- src/main/groovy/com/barneyb/magic/creator/compositor/Paragraph.groovy
+++ src/main/groovy/com/barneyb/magic/creator/compositor/Paragraph.groovy
@@ -1,8 +1,17 @@
 package com.barneyb.magic.creator.compositor
 
+import groovy.transform.Immutable
+
 /**
  *
  * @author bboisvert
  */
+@Immutable
 class Paragraph implements Renderable {
+
+    @Override
+    String toString() {
+        "\u00b6"
+    }
+
 }
