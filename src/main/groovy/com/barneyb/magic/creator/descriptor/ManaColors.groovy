9ae9193d6f297762dbbd5503c19a71a364fbf371
barneyb <bboisvert@gmail.com>
2014-06-23 08:40:51 -0700

    added COLORLESS to the Colors enum and renamed to ManaColors

--- src/main/groovy/com/barneyb/magic/creator/descriptor/ManaColors.groovy
+++ src/main/groovy/com/barneyb/magic/creator/descriptor/ManaColors.groovy
@@ -1,13 +1,14 @@
 package com.barneyb.magic.creator.descriptor
 
 /**
- * This enum lists the 5 colors of magic in their canonical order.  Any time
+ * This enum lists the 6 colors of mana in their canonical order.  Any time
  * multiple colors are used together (casting cost symbols, hybrid borders,
  * etc.), they should be ordered according to this enum's constants' order.
  *
  * @author bboisvert
  */
-enum Colors {
+enum ManaColors {
+    COLORLESS,
     WHITE,
     BLUE,
     BLACK,
