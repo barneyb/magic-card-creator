56b937826bc638f485930b92c0109b0584b6c714
barneyb <bboisvert@gmail.com>
2014-06-23 08:08:59 -0700

    added javadoc for why the Colors enum exists

--- src/main/groovy/com/barneyb/magic/creator/descriptor/Colors.groovy
+++ src/main/groovy/com/barneyb/magic/creator/descriptor/Colors.groovy
@@ -1,6 +1,9 @@
 package com.barneyb.magic.creator.descriptor
 
 /**
+ * This enum lists the 5 colors of magic in their canonical order.  Any time
+ * multiple colors are used together (casting cost symbols, hybrid borders,
+ * etc.), they should be ordered according to this enum's constants' order.
  *
  * @author bboisvert
  */
