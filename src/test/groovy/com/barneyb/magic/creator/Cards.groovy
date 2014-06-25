1bb84cbb71842d7be3ef18d7b8faa7dd5e32942c
barneyb <bboisvert@gmail.com>
2014-06-25 07:53:53 -0700

    added assertRenderModel, and put context messages in the subfield assertions of assertCard and assertCardSet

--- src/test/groovy/com/barneyb/magic/creator/Cards.groovy
+++ src/test/groovy/com/barneyb/magic/creator/Cards.groovy
@@ -1,4 +1,5 @@
 package com.barneyb.magic.creator
+
 import com.barneyb.magic.creator.asset.RemoteImage
 import com.barneyb.magic.creator.asset.RenderSet
 import com.barneyb.magic.creator.compositor.AbilityText
@@ -21,17 +22,35 @@ class Cards {
 
     static def _cl = Cards.classLoader
 
+    static assertRenderModel(RenderModel e, RenderModel a) {
+        try {
+            assertEquals(e, a)
+        } catch (AssertionError ae) {
+            assertEquals("frame doesn't match", e.frame, a.frame)
+            assertEquals("title doesn't match", e.title, a.title)
+            assertEquals("cost doesn't match", e.cost, a.cost)
+            assertEquals("artwork doesn't match", e.artwork, a.artwork)
+            assertEquals("type doesn't match", e.type, a.type)
+            assertEquals("body doesn't match", e.body, a.body)
+            assertEquals("powerToughness doesn't match", e.powerToughness, a.powerToughness)
+            assertEquals("whiteFooterText doesn't match", e.whiteFooterText, a.whiteFooterText)
+            assertEquals("artist doesn't match", e.artist, a.artist)
+            assertEquals("footer doesn't match", e.footer, a.footer)
+            throw ae
+        }
+    }
+
     static assertCard(Card e, Card a) {
         try {
-            assertEquals(e.title, a.title)
-            assertEquals(e.costString, a.costString)
-            assertEquals(e.artwork, a.artwork)
-            assertEquals(e.type, a.type)
-            assertEquals(e.subtype, a.subtype)
-            assertEquals(e.body, a.body)
-            assertEquals(e.power, a.power)
-            assertEquals(e.toughness, a.toughness)
-            assertEquals(e.artist, a.artist)
+            assertEquals("title doesn't match", e.title, a.title)
+            assertEquals("costString doesn't match", e.costString, a.costString)
+            assertEquals("artwork doesn't match", e.artwork, a.artwork)
+            assertEquals("type doesn't match", e.type, a.type)
+            assertEquals("subtype doesn't match", e.subtype, a.subtype)
+            assertEquals("body doesn't match", e.body, a.body)
+            assertEquals("power doesn't match", e.power, a.power)
+            assertEquals("toughness doesn't match", e.toughness, a.toughness)
+            assertEquals("artist doesn't match", e.artist, a.artist)
         } catch (AssertionError ae) {
             println "expected: $e"
             println "  actual: $a"
@@ -43,10 +62,10 @@ class Cards {
         try {
             assertEquals(expected, actual)
         } catch (AssertionError ae) {
-            assertEquals(expected.name, actual.name)
-            assertEquals(expected.footer, actual.footer)
-            assertEquals(expected.cardsInSet, actual.cardsInSet)
-            assertEquals(expected*.title, actual*.title)
+            assertEquals("name doesn't match", expected.name, actual.name)
+            assertEquals("footer doesn't match", expected.footer, actual.footer)
+            assertEquals("cardsInSet doesn't match", expected.cardsInSet, actual.cardsInSet)
+            assertEquals("titles don't match", expected*.title, actual*.title)
             expected.eachWithIndex { it, i ->
                 assertCard(it, actual.get(i))
             }
