8d9f347dd97654fddc68378a4d34e6481773a4bb
barneyb <bboisvert@gmail.com>
2014-06-27 08:18:05 -0700

    svg flow, though all it does is statically write the title in default font

---
+++ src/test/groovy/com/barneyb/magic/creator/compositor/svg/SvgCompositorTest.groovy
@@ -0,0 +1,79 @@
+package com.barneyb.magic.creator.compositor.svg
+import com.barneyb.magic.creator.Cards
+import com.barneyb.magic.creator.asset.AssetDescriptor
+import com.barneyb.magic.creator.asset.RenderSet
+import org.junit.Before
+import org.junit.BeforeClass
+import org.junit.Test
+/**
+ *
+ * @author bboisvert
+ */
+class SvgCompositorTest {
+
+    static final File outputDir = new File("build")
+
+    @BeforeClass
+    static void ensureTarget() {
+        if (! outputDir.exists()) {
+            outputDir.mkdir()
+        }
+        def f = new File(outputDir, "preview_svg.html")
+        f.text = """\
+<html>
+<head>
+<style>
+    img {
+        width: 400px;
+        height: 560px;
+    }
+</style>
+</head>
+<body>
+<embed src="sally.svg" />
+<embed src="counterspell.svg" />
+<embed src="nightmare.svg" />
+<embed src="blitz-hellion.svg" />
+<embed src="barney.svg" />
+</body>
+</html>"""
+    }
+
+    RenderSet rs
+
+    @Before
+    void _makeRenderSet() {
+        rs = AssetDescriptor.fromStream(SvgCompositorTest.classLoader.getResourceAsStream("assets/descriptor.json")).getRenderSet("screen")
+    }
+
+    @Test
+    void sally() {
+        def m = Cards.sally(rs)
+        new SvgCompositor().compose(m, rs, new File(outputDir, "sally.svg").newOutputStream())
+    }
+
+    @Test
+    void counterspell() {
+        def m = Cards.counterspell(rs)
+        new SvgCompositor().compose(m, rs, new File(outputDir, "counterspell.svg").newOutputStream())
+    }
+
+    @Test
+    void nightmare() {
+        def m = Cards.nightmare(rs)
+        new SvgCompositor().compose(m, rs, new File(outputDir, "nightmare.svg").newOutputStream())
+    }
+
+    @Test
+    void barney() {
+        def m = Cards.barney(rs)
+        new SvgCompositor().compose(m, rs, new File(outputDir, "barney.svg").newOutputStream())
+    }
+
+    @Test
+    void blitz_hellion() {
+        def m = Cards.hellion(rs)
+        new SvgCompositor().compose(m, rs, new File(outputDir, "blitz-hellion.svg").newOutputStream())
+    }
+
+}
