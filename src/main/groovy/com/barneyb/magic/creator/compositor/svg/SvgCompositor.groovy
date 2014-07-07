028a55f8f2bfca822358d6a10f14a3683f4f6e34
barneyb <bboisvert@gmail.com>
2014-07-06 19:45:03 -0700

    made use the renderset's frame dimensions, instead of hard-coded values

--- src/main/groovy/com/barneyb/magic/creator/compositor/svg/SvgCompositor.groovy
+++ src/main/groovy/com/barneyb/magic/creator/compositor/svg/SvgCompositor.groovy
@@ -13,7 +13,7 @@ class SvgCompositor implements Compositor {
     @Override
     void compose(RenderModel model, RenderSet rs, OutputStream dest) {
         def out = new PrintWriter(dest)
-        out.println('<svg xmlns="http://www.w3.org/2000/svg" width="400" height="560">')
+        out.println('<svg xmlns="http://www.w3.org/2000/svg" width="' + rs.frames.size.width + '" height="' + rs.frames.size.height + '">')
         out.println('<text x="10" y="40" fill="#000000">' + model.title + '</text>')
         out.println("</svg>")
         out.close()
