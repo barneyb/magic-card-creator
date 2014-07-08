d93faef53e504006419a16cd5a0cc6b0917363c1
barneyb <bboisvert@gmail.com>
2014-07-07 20:14:45 -0700

    moved the drop shadow on the large SVG icons over slightly so the "light" is more vertical, and the icons are narrow by a couple pixels

--- src/main/resources/assets/print/generate_icons.groovy
+++ src/main/resources/assets/print/generate_icons.groovy
@@ -12,10 +12,12 @@ class Icon {
 
     String getLarge() {
         """\
-<svg xmlns="http://www.w3.org/2000/svg" width="47" height="47">
+<svg xmlns="http://www.w3.org/2000/svg" width="45" height="47">
     <circle r="22" cx="22" cy="25" fill="#000" opacity="0.75" />
-    <circle r="22" cx="25" cy="22" fill="$color" />
-    $body
+    <circle r="22" cx="23" cy="22" fill="$color" />
+    <g transform="translate(-2 0)">
+        $body
+    </g>
 </svg>
 """
     }
