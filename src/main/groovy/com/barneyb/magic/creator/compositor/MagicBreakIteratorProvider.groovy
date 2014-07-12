12eb71e1bc7d9a5b6febb7bcd381a5b13d5253aa
barneyb <bboisvert@gmail.com>
2014-07-12 10:00:08 -0700

    implemented a custom line-breaking BreakIterator that accounts for power/toughness strings in text.  The core JDK doesn't provide access to it's RuleBasedBreakIterator for some unknown reason, so ICU is used instead.  Which is rather lame.

---
+++ src/main/groovy/com/barneyb/magic/creator/compositor/MagicBreakIteratorProvider.groovy
@@ -0,0 +1,27 @@
+package com.barneyb.magic.creator.compositor
+import com.ibm.icu.text.RuleBasedBreakIterator
+
+import java.text.BreakIterator
+/**
+ *
+ * @author bboisvert
+ */
+class MagicBreakIteratorProvider {
+
+    public static final String RULES
+
+    static {
+        def rules = new StringBuilder(RuleBasedBreakIterator.lineInstance.toString())
+        def i = rules.indexOf('!!forward;')
+        rules.insert(i, '$PowerToughness = [+-]?([0-9]+|[xX*])\\/[+-]?([0-9]+|[xX*]);')
+        i = rules.indexOf('!!reverse;')
+        rules.insert(i, '$PowerToughness {500};')
+        i = rules.indexOf('!!safe_reverse;')
+        RULES = rules.toString()
+    }
+
+    static BreakIterator getLineInstance() {
+        new ICUBreakIteratorAdapter(new RuleBasedBreakIterator(RULES))
+    }
+
+}
