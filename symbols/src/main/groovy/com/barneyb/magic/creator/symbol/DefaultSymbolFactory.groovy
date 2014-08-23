bc81110cb885c4932cbc16a098468f08e931c0f7
Barney Boisvert <bboisvert@gmail.com>
2014-08-23 11:48:37 -0700

    ensured casing and brace-wrapping are still handled correctly

--- symbols/src/main/groovy/com/barneyb/magic/creator/symbol/DefaultSymbolFactory.groovy
+++ symbols/src/main/groovy/com/barneyb/magic/creator/symbol/DefaultSymbolFactory.groovy
@@ -13,30 +13,33 @@ class DefaultSymbolFactory implements SymbolFactory {
 
     @Override
     Symbol getSymbol(String symbol) {
-        symbol = symbol.toLowerCase()
+        symbol = symbol.toUpperCase()
+        if (symbol.startsWith('{') && symbol.endsWith('}')) {
+            symbol = symbol.substring(1, symbol.length() - 1)
+        }
         if (symbol.matches(~/\d\d?/)) {
             new DefaultSymbol(symbol, [ManaColor.COLORLESS])
-        } else if (symbol == 'x') {
+        } else if (symbol == 'X') {
             new DefaultSymbol(symbol, [ManaColor.COLORLESS])
-        } else if (symbol.matches(~/[wubrg]\/[wubrg]/)) {
+        } else if (symbol.matches(~/[WUBRG]\/[WUBRG]/)) {
             new DefaultSymbol(symbol, [ManaColor.fromSymbol(symbol.substring(0, 1)), ManaColor.fromSymbol(symbol.substring(2, 3))])
-        } else if (symbol.matches(~/2\/[wubrg]/)) {
+        } else if (symbol.matches(~/2\/[WUBRG]/)) {
             new DefaultSymbol(symbol, [ManaColor.COLORLESS, ManaColor.fromSymbol(symbol.substring(2, 3))])
-        } else if (symbol.matches(~/[wubrg]\/p/)) {
+        } else if (symbol.matches(~/[WUBRG]\/P/)) {
             new DefaultSymbol(symbol, [ManaColor.fromSymbol(symbol.substring(0, 1))])
-        } else if (symbol == 'w') {
+        } else if (symbol == 'W') {
             new DefaultSymbol(symbol, [ManaColor.WHITE])
-        } else if (symbol == 'u') {
+        } else if (symbol == 'U') {
             new DefaultSymbol(symbol, [ManaColor.BLUE])
-        } else if (symbol == 'b') {
+        } else if (symbol == 'B') {
             new DefaultSymbol(symbol, [ManaColor.BLACK])
-        } else if (symbol == 'r') {
+        } else if (symbol == 'R') {
             new DefaultSymbol(symbol, [ManaColor.RED])
-        } else if (symbol == 'g') {
+        } else if (symbol == 'G') {
             new DefaultSymbol(symbol, [ManaColor.GREEN])
-        } else if (symbol == 't') {
+        } else if (symbol == 'T') {
             new DefaultSymbol(symbol, [])
-        } else if (symbol == 'q') {
+        } else if (symbol == 'Q') {
             new DefaultSymbol(symbol, [])
         } else {
             throw new IllegalArgumentException("No '$symbol' symbol is known.")
--- symbols/src/test/groovy/com/barneyb/magic/creator/symbol/DefaultSymbolFactoryTest.groovy
+++ symbols/src/test/groovy/com/barneyb/magic/creator/symbol/DefaultSymbolFactoryTest.groovy
@@ -3,6 +3,8 @@ package com.barneyb.magic.creator.symbol
 import org.junit.Before
 import org.junit.Test
 
+import static org.junit.Assert.assertEquals
+
 /**
  *
  *
@@ -18,10 +20,22 @@ class DefaultSymbolFactoryTest {
     }
 
     @Test
+    void casing() {
+        assertEquals('U', factory.getSymbol("u").symbol)
+        assertEquals('U', factory.getSymbol("U").symbol)
+    }
+
+    @Test
+    void braces() {
+        assertEquals('{U}', factory.getSymbol("U").toString())
+        assertEquals('{U}', factory.getSymbol("{U}").toString())
+    }
+
+    @Test
     void noErrors() {
         [
             "colored": ['w', 'u', 'b', 'r', 'g'],
-            "colorless": (0..20)*.toString() + ['x'],
+            "colorless": ['x'] + (0..20)*.toString(),
             "tap": ['t', 'q'],
             "hybrid": ['w/u', 'w/b', 'u/b', 'u/r', 'b/r', 'b/g', 'r/g', 'r/w', 'g/w', 'g/u'],
             "mono-hybrid": ['2/w', '2/u', '2/b', '2/r', '2/g'],
@@ -29,7 +43,7 @@ class DefaultSymbolFactoryTest {
         ].each { n, is ->
             println n
             is.each {
-                println "  $it : ${factory.getSymbol(it.toLowerCase())} : ${factory.getSymbol(it.toUpperCase())}"
+                println "  $it : ${factory.getSymbol(it.toLowerCase())} : ${factory.getSymbol('{' + it.toUpperCase() + '}')}"
             }
         }
     }
