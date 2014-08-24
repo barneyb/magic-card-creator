8d5c9941ed0779d8806d9839eafdf613bab06e68
Barney Boisvert <bboisvert@gmail.com>
2014-08-23 20:52:08 -0700

    set up processing of the frame velocity template into an SVGDocument that can be laid out atop of.  it doesn't really do the rendering, just goes through the pipeline

--- theme/src/main/groovy/com/barneyb/magic/creator/theme/DefaultLayout.groovy
+++ theme/src/main/groovy/com/barneyb/magic/creator/theme/DefaultLayout.groovy
@@ -3,6 +3,11 @@ package com.barneyb.magic.creator.theme
 import com.barneyb.magic.creator.api.Card
 import com.barneyb.magic.creator.api.Layout
 import com.barneyb.magic.creator.api.LayoutType
+import com.barneyb.magic.creator.util.XmlUtils
+import org.apache.velocity.VelocityContext
+import org.apache.velocity.app.VelocityEngine
+import org.apache.velocity.runtime.RuntimeConstants
+import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader
 import org.w3c.dom.svg.SVGDocument
 
 /**
@@ -20,7 +25,19 @@ class DefaultLayout implements Layout {
 
     @Override
     SVGDocument layout(Card card) {
-        return null
+        def ve = new VelocityEngine()
+        ve.setProperty(RuntimeConstants.RESOURCE_LOADER, "classpath");
+        ve.setProperty("classpath.resource.loader.class", ClasspathResourceLoader.class.getName());
+        ve.init()
+        def t = ve.getTemplate("theme/default/frame.svg.vm")
+        def ctx = new VelocityContext([
+            spec: new FrameSpec(type, card)
+        ])
+        def writer = new StringWriter()
+        t.merge(ctx, writer)
+        def doc = XmlUtils.read(writer.toString())
+        // todo: do the card layout
+        doc
     }
 
 }
