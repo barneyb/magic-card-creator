6b3db39c29a6084e651f023d45c88ddbce364bee
Barney Boisvert <bboisvert@gmail.com>
2014-08-27 14:52:02 -0700

    moved leveling into CreatureCard

--- api/src/main/groovy/com/barneyb/magic/creator/api/CreatureCard.groovy
+++ api/src/main/groovy/com/barneyb/magic/creator/api/CreatureCard.groovy
@@ -6,8 +6,34 @@ package com.barneyb.magic.creator.api
  */
 interface CreatureCard extends Card {
 
+    static interface Level {
+
+        /**
+         * The start of the range this level applies to.  If null, <tt>"0"</tt>
+         * is assumed, which is silly (because that'd be the default state).
+         */
+        String getStart()
+
+        /**
+         * The end of the range this level applies to.  If null, it is assumed
+         * to be unbounded.
+         */
+        String getEnd()
+
+        String getPower()
+
+        String getToughness()
+
+        List<List<BodyItem>> getRulesText()
+
+    }
+
     String getPower()
 
     String getToughness()
+
+    boolean isLeveler()
+
+    List<Level> getLevels()
 
 }
