87f6df05d3687102fd038c78e0184ab180907c53
Barney Boisvert <bboisvert@gmail.com>
2014-08-27 08:39:29 -0700

    added a "test" that just dumps the usage information for the CLI so 'mvn test' can succeed directly out of Git

---
+++ cli/src/test/groovy/com/barneyb/magic/creator/cli/CardCreatorTest.groovy
@@ -0,0 +1,17 @@
+package com.barneyb.magic.creator.cli
+
+import org.junit.Test
+
+/**
+ *
+ *
+ * @author barneyb
+ */
+class CardCreatorTest {
+
+    @Test
+    void showHelp() {
+        CardCreator.main("help")
+    }
+
+}
