702f353b47f4042e67992e8776e9c634592c9cb1
Barney Boisvert <bboisvert@gmail.com>
2014-08-31 16:48:41 -0700

    changed CardSetIconFactory to also use Icon instead of raw SVGDocument

--- api/src/main/groovy/com/barneyb/magic/creator/api/CardSetIconFactory.groovy
+++ api/src/main/groovy/com/barneyb/magic/creator/api/CardSetIconFactory.groovy
@@ -1,7 +1,4 @@
 package com.barneyb.magic.creator.api
-
-import org.w3c.dom.svg.SVGDocument
-
 /**
  * I am in charge of converting set keys (as Strings) into SVGDocuments that
  * visually represent them.  All documents will contain a single
@@ -14,11 +11,11 @@ interface CardSetIconFactory {
     /**
      * I construct the icon for the named set assuming the COMMON rarity.
      */
-    SVGDocument getIcon(String key)
+    Icon getIcon(String key)
 
     /**
      * I construct the icon for the named set at the specified rarity.
      */
-    SVGDocument getIcon(String key, Rarity rarity)
+    Icon getIcon(String key, Rarity rarity)
 
 }
