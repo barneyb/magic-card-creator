70bfe59b99f5a97c8dc03cfc969a7dd7a75055e9
Barney Boisvert <bboisvert@gmail.com>
2014-09-04 10:46:23 -0700

    filled out the rest of the ColorTheme map, though everything is just the metal texture with a tinted flood with colors I completely guessed on

--- theme/src/main/groovy/com/barneyb/magic/creator/theme/DefaultTheme.groovy
+++ theme/src/main/groovy/com/barneyb/magic/creator/theme/DefaultTheme.groovy
@@ -27,19 +27,12 @@ class DefaultTheme implements Theme {
 
     protected SymbolIconFactory iconFactory = new DefaultIconFactory()
 
-//    <pattern id="enchantmentFrame" patternUnits="userSpaceOnUse" height="480" width="640">
-//        <image width="640" height="480" xlink:href="starfield.jpg" opacity=".6" />
-//    </pattern>
     protected Texture semiEnchantmentTexture = new SimpleTexture(
         image: new ClasspathRasterImage("theme/default/starfield.jpg"),
         opacity: 0.6f
     )
 
-//    <pattern id="bar" patternUnits="userSpaceOnUse" height="240" width="240">
-//        <image x="-10" y="-10" width="256" height="256" xlink:href="metal.jpg" />
-//        <rect width="240" height="240" fill="white" opacity=".3" />
-//    </pattern>
-    protected Texture barTexture = new SimpleTexture(
+    protected SimpleTexture barTexture = new SimpleTexture(
         image: new ClasspathRasterImage("theme/default/metal.jpg"),
         overFlood: new SimpleFlood(Color.WHITE, 0.3f),
         bounds: new Rectangle(10, 10, 240, 240)
@@ -48,38 +41,57 @@ class DefaultTheme implements Theme {
     protected Map<ThemedColor, ? extends ColorTheme> colors = [
         (ThemedColor.WHITE): new SimpleColorTheme(
             baseColor: ColorUtils.fromHex("#ffffff"),
-//            <pattern id="whiteFrame" patternUnits="userSpaceOnUse" height="600" width="600">
-//                <image width="600" height="600" xlink:href="white.jpg" />
-//            </pattern>
             frameTexture: new SimpleTexture(new ClasspathRasterImage("theme/default/white.jpg")),
             barTexture: barTexture,
-//            <pattern id="whiteTextbox" patternUnits="userSpaceOnUse" height="480" width="640">
-//                <image width="640" height="480" xlink:href="cloud.jpg" />
-//                <rect width="640" height="480" fill="#fffafc" opacity=".8" />
-//            </pattern>
             textboxTexture: new SimpleTexture(
                 image: new ClasspathRasterImage("theme/default/cloud.jpg"),
                 overFlood: new SimpleFlood(ColorUtils.fromHex("#fffafc"), 0.8f)
             ),
         ),
+        (ThemedColor.BLUE): new SimpleColorTheme(
+            baseColor: ColorUtils.fromHex("#9FC5FF"),
+            frameTexture: barTexture.floodOver(new SimpleFlood(ColorUtils.fromHex("#D9E8FF"), 0.6f)),
+            barTexture: barTexture,
+            textboxTexture: barTexture.floodOver(new SimpleFlood(ColorUtils.fromHex("#D9E8FF"), 0.8f)),
+        ),
+        (ThemedColor.BLACK): new SimpleColorTheme(
+            baseColor: ColorUtils.fromHex("#666666"),
+            frameTexture: barTexture.floodOver(new SimpleFlood(ColorUtils.fromHex("#A6A6A6"), 0.6f)),
+            barTexture: barTexture,
+            textboxTexture: barTexture.floodOver(new SimpleFlood(ColorUtils.fromHex("#A6A6A6"), 0.8f)),
+        ),
         (ThemedColor.RED): new SimpleColorTheme(
             baseColor: ColorUtils.fromHex("#ff0000"),
-//            <pattern id="redFrame" patternUnits="userSpaceOnUse" height="400" width="640">
-//                <image width="640" height="400" xlink:href="red.jpg" />
-//            </pattern>
             frameTexture: new SimpleTexture(new ClasspathRasterImage("theme/default/red.jpg")),
             barTexture: barTexture,
-//            <pattern id="redTextbox" patternUnits="userSpaceOnUse" height="400" width="640">
-//                <image width="640" height="400" xlink:href="red.jpg" />
-//                <rect width="640" height="400" fill="#fee" opacity=".8" />
-//            </pattern>
             textboxTexture: new SimpleTexture(
                 image: new ClasspathRasterImage("theme/default/red.jpg"),
                 overFlood: new SimpleFlood(ColorUtils.fromHex("#ffeeee"), 0.8f)
             ),
         ),
-        (ThemedColor.LAND): new SimpleColorTheme(
+        (ThemedColor.GREEN): new SimpleColorTheme(
+            baseColor: ColorUtils.fromHex("#A2D192"),
+            frameTexture: barTexture.floodOver(new SimpleFlood(ColorUtils.fromHex("#BDD1B6"), 0.6f)),
             barTexture: barTexture,
+            textboxTexture: barTexture.floodOver(new SimpleFlood(ColorUtils.fromHex("#BDD1B6"), 0.8f)),
+        ),
+        (ThemedColor.GOLD): new SimpleColorTheme(
+            baseColor: ColorUtils.fromHex("#E8E78D"),
+            frameTexture: barTexture.floodOver(new SimpleFlood(ColorUtils.fromHex("#E8E8B7"), 0.6f)),
+            barTexture: barTexture,
+            textboxTexture: barTexture.floodOver(new SimpleFlood(ColorUtils.fromHex("#E8E8B7"), 0.8f)),
+        ),
+        (ThemedColor.ARTIFACT): new SimpleColorTheme(
+            baseColor: ColorUtils.fromHex("#AAD7EC"),
+            frameTexture: barTexture.floodOver(new SimpleFlood(ColorUtils.fromHex("#C4DFEC"), 0.6f)),
+            barTexture: barTexture,
+            textboxTexture: barTexture.floodOver(new SimpleFlood(ColorUtils.fromHex("#C4DFEC"), 0.8f)),
+        ),
+        (ThemedColor.LAND): new SimpleColorTheme(
+            baseColor: ColorUtils.fromHex("#FFD798"),
+            frameTexture: barTexture.floodOver(new SimpleFlood(ColorUtils.fromHex("#FFEED4"), 0.6f)),
+            barTexture: barTexture,
+            textboxTexture: barTexture.floodOver(new SimpleFlood(ColorUtils.fromHex("#FFEED4"), 0.8f)),
         ),
     ]
 
