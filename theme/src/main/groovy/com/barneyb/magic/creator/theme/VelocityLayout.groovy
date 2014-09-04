21036bf59022db7be2f773ad8a399b6891b7338b
Barney Boisvert <bboisvert@gmail.com>
2014-09-04 16:55:02 -0700

    comment about url.resource.loader.root

--- theme/src/main/groovy/com/barneyb/magic/creator/theme/VelocityLayout.groovy
+++ theme/src/main/groovy/com/barneyb/magic/creator/theme/VelocityLayout.groovy
@@ -24,10 +24,10 @@ class VelocityLayout {
 
     SVGDocument layout(Card card) {
         def ve = new VelocityEngine()
-        ve.setProperty(RuntimeConstants.RESOURCE_LOADER, "classpath, url");
-        ve.setProperty("classpath.resource.loader.class", ClasspathResourceLoader.getName());
-        ve.setProperty("url.resource.loader.class", URLResourceLoader.getName());
-        ve.setProperty("url.resource.loader.root", "");
+        ve.setProperty(RuntimeConstants.RESOURCE_LOADER, "classpath, url")
+        ve.setProperty("classpath.resource.loader.class", ClasspathResourceLoader.getName())
+        ve.setProperty("url.resource.loader.class", URLResourceLoader.getName())
+        ve.setProperty("url.resource.loader.root", "") // without this, even though unused, the loader won't be active.
         ve.setProperty("runtime.references.strict", true)
         ve.init()
         def t = ve.getTemplate(template)
