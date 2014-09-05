176abeca94df05ae67026145d2ae4f0c4a85b302
Barney Boisvert <bboisvert@gmail.com>
2014-09-05 10:51:33 -0700

    created a FontLoader class that can load fonts into the JVM's graphics environment from the classpath to augment what the system already has installed

---
+++ utils/src/main/groovy/com/barneyb/magic/creator/util/FontLoader.groovy
@@ -0,0 +1,27 @@
+package com.barneyb.magic.creator.util
+
+import java.awt.Font
+import java.awt.GraphicsEnvironment
+
+/**
+ *
+ *
+ * @author barneyb
+ */
+class FontLoader {
+
+    static void fromClasspath(String... paths) {
+        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment()
+        def cl = getClass().classLoader
+        paths.each {
+            def type
+            if (it.endsWith("ttf") || it.endsWith("TTF")) {
+                type = Font.TRUETYPE_FONT
+            } else {
+                throw new IllegalArgumentException("Unknown font type: $it")
+            }
+            ge.registerFont(Font.createFont(type, cl.getResourceAsStream(it)))
+        }
+    }
+
+}
