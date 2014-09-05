2d5ca93ba0fdbbe1f4c94e47b945e5996f6b3093
Barney Boisvert <bboisvert@gmail.com>
2014-09-05 08:28:18 -0700

    made TextBox extend Rectangle2D.Double instead of Rectangle, and added a method to subtract a Dimension2D

--- theme/src/main/groovy/com/barneyb/magic/creator/theme/TextBox.groovy
+++ theme/src/main/groovy/com/barneyb/magic/creator/theme/TextBox.groovy
@@ -1,22 +1,21 @@
 package com.barneyb.magic.creator.theme
-
 import groovy.transform.Canonical
 import groovy.transform.EqualsAndHashCode
 import groovy.transform.TupleConstructor
 
 import java.awt.Font
-import java.awt.Rectangle
 import java.awt.font.TextAttribute
+import java.awt.geom.Dimension2D
+import java.awt.geom.Rectangle2D
 
 import static java.awt.font.TextAttribute.*
-
 /**
  * Created by barneyb on 7/27/2014.
  */
 @TupleConstructor(includeSuperFields = true)
 @EqualsAndHashCode(callSuper = true)
 @Canonical
-class TextBox extends Rectangle {
+class TextBox extends Rectangle2D.Double {
 
     String family = 'Comic Sans MS' // this might not work on non-Windows
 
@@ -44,6 +43,10 @@ class TextBox extends Rectangle {
             (FAMILY): family,
             (WEIGHT): bold ? WEIGHT_BOLD : WEIGHT_REGULAR,
         ]
+    }
+
+    TextBox minus(Dimension2D dim) {
+        new TextBox(x, y, width - dim.width, height - dim.height, family, bold)
     }
 
 }
