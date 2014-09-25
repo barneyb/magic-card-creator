59c15e9d29a9067d6d9b130f2f86d6194fdc4268
Barney Boisvert <bboisvert@gmail.com>
2014-09-25 10:30:40 -0700

    converted the --print option for 'compose' to a --bleed option on 'pdf'

--- cli/src/main/groovy/com/barneyb/magic/creator/cli/ComposeCommand.groovy
+++ cli/src/main/groovy/com/barneyb/magic/creator/cli/ComposeCommand.groovy
@@ -27,9 +27,6 @@ class ComposeCommand extends BaseDescriptorCommand implements Executable {
     @Parameter(names = "--theme-descriptor", description = "A theme descriptor file, if needed")
     File themeDescriptor
 
-    @Parameter(names = "--print", description = "If true, composed cards will be bled for printing")
-    boolean print = false
-
     void execute(MainCommand main, JCommander jc) {
         if (! outputDir.exists()) {
             outputDir.mkdirs()
@@ -55,39 +52,6 @@ class ComposeCommand extends BaseDescriptorCommand implements Executable {
             def file = new File(outputDir, it.cardNumber + ".svg")
             try {
                 def doc = theme.layout(it)
-                if (print) {
-                    def xbleed = 17.5
-                    def ybleed = 17.5
-                    def rotate = 0 // todo: setting this to anything but 0 mod 360 screws up the filters and patterns.  no idea why.
-                    def size = SvgUtils.size(doc)
-                    def base = doc
-                    doc = XmlUtils.create()
-
-                    def w = size.width
-                    def h = size.height
-                    def flip = rotate % 180 != 0
-                    def newWidth = (flip ? h : w) + xbleed * 2
-                    def newHeight = (flip ? w : h) + ybleed * 2
-
-                    XmlUtils.elattr(doc.rootElement, [
-                        width: newWidth,
-                        height: newHeight
-                    ])
-                    XmlUtils.el(doc.rootElement, 'rect', [
-                        width: "100%",
-                        height: "100%",
-                        fill: "#000"
-                    ])
-                    def g = XmlUtils.el(doc.rootElement, 'g', [
-                        transform: "translate($xbleed $ybleed)"
-                    ])
-                    if (rotate % 360 != 0) {
-                        g = XmlUtils.el(g, 'g', [
-                            transform: (flip ? "translate(${(h - w) / 2} ${(w - h) / 2}) " : "") + "rotate($rotate ${(float) w / 2} ${(float) h / 2})"
-                        ])
-                    }
-                    g.appendChild(doc.adoptNode(base.rootElement))
-                }
                 def out = file.newWriter()
                 XmlUtils.write(doc, out)
                 out.close()
--- cli/src/main/groovy/com/barneyb/magic/creator/cli/PdfCommand.groovy
+++ cli/src/main/groovy/com/barneyb/magic/creator/cli/PdfCommand.groovy
@@ -30,6 +30,59 @@ class PdfCommand extends BaseTranscodeCommand {
     @Parameter(names = "--rotate", description = "how to rotate the cards when transcoding (none, cw, ccw, flip)")
     Rotate rotate = Rotate.none
 
+    @Parameter(names = "--bleed", description = "if true, add a print bleed when transcoding")
+    boolean bleed = false
+
+    @Override
+    protected File transformSource(File src) {
+        if (bleed) {
+            def doc = XmlUtils.read(src.newReader())
+            doc = addBleed(doc)
+            def ns = new File(src.parentFile, src.name.contains('.') ? src.name.replaceFirst(/\./, '-bleed.') : src.name + "-bleed")
+            ns.deleteOnExit()
+            def out = ns.newWriter()
+            XmlUtils.write(doc, out)
+            out.close()
+            src = ns
+        }
+        src
+    }
+
+    protected SVGDocument addBleed(SVGDocument doc) {
+        def xbleed = 17.5
+        def ybleed = 17.5
+        def rotate = 0 // todo: setting this to anything but 0 mod 360 screws up the filters and patterns.  no idea why.
+        def size = SvgUtils.size(doc)
+        def base = doc
+        doc = XmlUtils.create()
+
+        def w = size.width
+        def h = size.height
+        def flip = rotate % 180 != 0
+        def newWidth = (flip ? h : w) + xbleed * 2
+        def newHeight = (flip ? w : h) + ybleed * 2
+
+        XmlUtils.elattr(doc.rootElement, [
+            width : newWidth,
+            height: newHeight
+        ])
+        XmlUtils.el(doc.rootElement, 'rect', [
+            width : "100%",
+            height: "100%",
+            fill  : "#000"
+        ])
+        def g = XmlUtils.el(doc.rootElement, 'g', [
+            transform: "translate($xbleed $ybleed)"
+        ])
+        if (rotate % 360 != 0) {
+            g = XmlUtils.el(g, 'g', [
+                transform: (flip ? "translate(${(h - w) / 2} ${(w - h) / 2}) " : "") + "rotate($rotate ${(float) w / 2} ${(float) h / 2})"
+            ])
+        }
+        g.appendChild(doc.adoptNode(base.rootElement))
+        doc
+    }
+
     @Override
     protected String getUrlPath() {
         "/convert/pdf?r=$rotate.direction"
